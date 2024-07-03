94f8038dc1bb4960e0c42cbdc93c97b16eb08de7
fuadadhim24 <fuadadhim24@gmail.com>
2024-07-03 21:10:22 +0700

    feat: add theme

---
+++ lib/page/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shared/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color greenColor = const Color(0xff00B074);
+Color greenTransparentColor = const Color(0xff00B074).withOpacity(0.15);
+Color whiteColor = const Color(0xffFFFFFF);
+Color redColor = const Color(0xffFF5B5B);
+Color redTransparentColor = const Color(0xffFF5B5B).withOpacity(0.15);
+
+TextStyle whiteTS = GoogleFonts.inter(
+  color: whiteColor,
+);
+TextStyle greenTS = GoogleFonts.inter(
+  color: greenColor,
+);
+TextStyle redTS = GoogleFonts.inter(
+  color: redColor,
+);
+
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
